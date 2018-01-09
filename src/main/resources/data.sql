bab813757f283be70b06cd56a99598fe82044537
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-01-09 16:31:27 +0100

    Initialize data with Spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Cup');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Pinch');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Ounce');
