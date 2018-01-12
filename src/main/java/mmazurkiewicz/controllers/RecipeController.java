d310989b38b401f49715c302ffa55fda322d92b4
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-01-12 12:06:41 +0100

    Add new recipe page and handle it

--- src/main/java/mmazurkiewicz/controllers/RecipeController.java
+++ src/main/java/mmazurkiewicz/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package mmazurkiewicz.controllers;
 
+import mmazurkiewicz.commands.RecipeCommand;
 import mmazurkiewicz.services.RecipesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +24,20 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipesService.findById(new Long(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeForm";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipesService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
