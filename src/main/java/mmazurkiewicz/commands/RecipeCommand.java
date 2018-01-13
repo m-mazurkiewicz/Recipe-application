9db8474fe83aa63096d77c1b2c8b90f97bcaa8d1
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-01-14 00:12:54 +0100

    Validate form for Recipe

--- src/main/java/mmazurkiewicz/commands/RecipeCommand.java
+++ src/main/java/mmazurkiewicz/commands/RecipeCommand.java
@@ -7,7 +7,12 @@ import mmazurkiewicz.domain.Category;
 import mmazurkiewicz.domain.Difficulty;
 import mmazurkiewicz.domain.Ingredient;
 import mmazurkiewicz.domain.Notes;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,12 +21,28 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(0)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(99)
     private Integer servings;
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
