2b856f193e56ccf24df4c0014c37737ef5ce3542
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-01-13 19:30:45 +0100

    Add specific view for 404 error

--- src/main/java/mmazurkiewicz/controllers/RecipeController.java
+++ src/main/java/mmazurkiewicz/controllers/RecipeController.java
@@ -2,11 +2,14 @@ package mmazurkiewicz.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import mmazurkiewicz.commands.RecipeCommand;
+import mmazurkiewicz.exceptions.NotFoundException;
 import mmazurkiewicz.services.RecipesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -52,5 +55,17 @@ public class RecipeController {
         recipesService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found error");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
--- src/test/java/mmazurkiewicz/controllers/RecipeControllerTest.java
+++ src/test/java/mmazurkiewicz/controllers/RecipeControllerTest.java
@@ -63,7 +63,8 @@ public class RecipeControllerTest {
         when(recipesService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 
     @Test
